220ce2565c9fa1ce4ce09c7a50b731fffe607407
R4ndomizeR <krainev106@gmail.com>
2022-07-31 04:03:11 +0300

    upd

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
 const projects = [
   {
-    id: 1,
     title: 'Game UI',
     url: 'http://demo.spcourse.ru/game-ui/'
   },
   {
-    id: 2,
     title: 'Solitaire',
     url: 'https://r4ndomizer.github.io/KlondikeSolitaire/'
   },
-  // {
-  //   id: 3,
-  //   title: 'Minesweeper',
-  //   url: 'https://r4ndomizer.github.io/Minesweeper/'
-  // },
+  {
+    title: 'SpaceX',
+    url: 'https://r4ndomizer.github.io/SpaceX/'
+  },
+  {
+    title: 'Minesweeper',
+    url: 'https://r4ndomizer.github.io/Minesweeper/'
+  }
 ]
 
 const socials = [
@@ -32,12 +33,8 @@ const socials = [
 ]
 
 function toggleCollapse(state = true) {
-  console.log(['toggleCollapse', state])
-
   const cardEl = document.getElementById('page__main')
   if (!cardEl) return
-
-  console.log(['toggleCollapse2', state])
 
   cardEl.classList.toggle('page__main--collapsed', state)
   cardEl.classList.toggle('page__main--opened', !state)
@@ -92,17 +89,14 @@ function renderItems() {
   const tabEl = document.getElementById('page__main-tab')
 
   if (tabEl) {
-    console.log(tabEl)
     tabEl.addEventListener('click', () => toggleCollapse(false))
   }
 
-  const frameEl = document.getElementById('page__frame')
-  if (frameEl) {
-    frameEl.onload = function () {
-      console.log('loaded frame')
-      // toggleCollapse(true)
-    }
-  }
+  // const frameEl = document.getElementById('page__frame')
+  // if (frameEl) {
+  // frameEl.onload = function () {
+  // console.log('loaded frame')
+  // toggleCollapse(true)
 
 }
 
